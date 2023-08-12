fe26c342421ec565fe0bdf8fac4b06e80cb748ca
kjuiop <copying_y@naver.com>
2023-08-12 15:28:13 +0900

    [#1] feat: api handler add, /api/health-check api add

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"catchreview-api-app/config"
+	"catchreview-api-app/handler"
+	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -16,17 +22,44 @@ func main() {
 		return
 	}
 
+	a := handler.NewApiHandler(cfg)
+
 	router := gin.Default()
-	router.GET("/api/health-check")
+	router.GET("/api/health-check", a.HealthCheck)
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", cfg.ApiPort),
 		Handler: router,
 	}
 
-	go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
-		}
-	}()
+	go serveHttpServer(srv)
+
+	quit := make(chan os.Signal, 1)
+	defer close(quit)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGKILL)
+
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 5 seconds.")
+	}
+	log.Println("Server exiting")
 }
+
+func serveHttpServer(srv *http.Server) {
+
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("listen: %s\n", err)
+		return
+	}
+}
+
