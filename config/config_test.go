1bb4ff4840b5c5b4d24fc16b0bbe58fc08799178
kjuiop <copying_y@naver.com>
2023-08-17 13:59:35 +0900

    [#2] test: config with env test

--- config/config_test.go
+++ config/config_test.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
@@ -10,4 +11,16 @@ func TestConfInitialize(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, "8088", config.ApiPort)
+}
+
+func TestConfWithEnv(t *testing.T) {
+
+	_ = os.Setenv("API_PORT", "9090")
+
+	config, err := ConfInitialize()
+	assert.NoError(t, err) // 에러 없어야 함
+
+	assert.Equal(t, "9090", config.ApiPort) // 환경 변수 값 확인
+
+	_ = os.Unsetenv("API_PORT")
 }
