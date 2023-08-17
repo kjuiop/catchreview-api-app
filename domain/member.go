f45588cd7dfa85a77c867ff1f0cef13bcfc5e323
kjuiop <copying_y@naver.com>
2023-08-17 16:53:42 +0900

    [#9] feat: Member domain create

---
+++ domain/member.go
@@ -0,0 +1,20 @@
+package domain
+
+import (
+	"context"
+	"time"
+)
+
+type Member struct {
+	Username        string    `json:"username"`
+	Password        string    `json:"password"`
+	Nickname        string    `json:"nickname"`
+	PrivacyAgreedAt time.Time `json:"privacy_agreed_at"`
+	PolicyAgreedAt  time.Time `json:"policy_agreed_at"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
+}
+
+type MemberUsecase interface {
+	Store(context.Context, *Member) error
+}
