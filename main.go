ab74d64579535201552880f35fc562efad186530
kjuiop <copying_y@naver.com>
2023-08-17 17:01:17 +0900

    [#9] refactor: handler 의 serve, close goroutine 함수 분리

--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"catchreview-api-app/config"
-	http2 "catchreview-api-app/internal/delivery/http"
+	"catchreview-api-app/internal/delivery/http/handler"
 	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
+	"time"
 )
 
 func main() {
@@ -29,10 +31,10 @@ func main() {
 		return
 	}
 
-	a := http2.NewApiHandler(cfg)
+	h := handler.NewApiHandler(cfg)
 
 	router := gin.Default()
-	router.GET("/api/health-check", a.HealthCheck)
+	router.GET("/api/health-check", h.HealthCheck)
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", cfg.ApiPort),
@@ -40,10 +42,43 @@ func main() {
 	}
 
 	wg.Add(1)
-	go a.CloseWithContext(ctx, cancel, srv, quit, &wg)
+	go closeWithContext(ctx, cancel, srv, quit, &wg)
 
 	wg.Add(1)
-	go a.ServeHttpServer(ctx, srv, &wg)
+	go serveHttpServer(ctx, srv, &wg)
 
 	wg.Wait()
+}
+
+func closeWithContext(ctx context.Context, cancel context.CancelFunc, srv *http.Server, quit chan os.Signal, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		select {
+		case <-quit:
+			log.Printf("Received exit signal: %v\n", quit)
+			cancel()
+		case <-ctx.Done():
+			log.Println("Context done, initiating graceful shutdown...")
+
+			if err := srv.Shutdown(ctx); err != nil {
+				log.Println("Server shutdown error:", err)
+				return
+			}
+			log.Println("Server gracefully stopped")
+			return
+		default:
+			time.Sleep(time.Second * 1)
+		}
+	}
+}
+
+func serveHttpServer(ctx context.Context, srv *http.Server, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	log.Println("ServeHttpServer in")
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("listen: %s\n", err)
+		return
+	}
 }
--- main_test.go
+++ main_test.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"catchreview-api-app/config"
+	"catchreview-api-app/internal/delivery/http/handler"
+	"context"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"testing"
 	"time"
@@ -34,4 +37,79 @@ func TestMainFunction(t *testing.T) {
 	time.Sleep(time.Second)
 
 	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
+
+func TestApiHandler_CloseWithContext(t *testing.T) {
+
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+
+	cfg := &config.Config{ApiPort: "0"}
+	ctx, cancel := context.WithCancel(context.Background())
+
+	server := &http.Server{
+		Addr:    ":" + cfg.ApiPort,
+		Handler: router,
+	}
+
+	wg := sync.WaitGroup{}
+	// 테스트: 핸들러의 CloseWithContext 호출
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		// 1초 후 종료 시그널 전송
+		time.Sleep(time.Second * 1)
+		quit <- syscall.SIGINT
+	}()
+
+	wg.Add(1)
+	go closeWithContext(ctx, cancel, server, quit, &wg)
+
+	wg.Wait() // 모든 고루틴이 종료될 때까지 대기
+
+	assert.ErrorIs(t, context.Canceled, ctx.Err())
+}
+
+func TestApiHandler_ServeHttpServer(t *testing.T) {
+
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+
+	cfg := &config.Config{ApiPort: "8098"}
+	ctx, cancel := context.WithCancel(context.Background())
+	h := handler.NewApiHandler(cfg)
+	router.GET("/health", h.HealthCheck)
+
+	server := &http.Server{
+		Addr:    ":" + cfg.ApiPort,
+		Handler: router,
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go serveHttpServer(ctx, server, &wg)
+
+	// 테스트용 HTTP 클라이언트 생성
+	client := http.Client{}
+	req, _ := http.NewRequest("GET", "http://localhost:"+cfg.ApiPort+"/health", nil)
+	resp, err := client.Do(req)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		// 1초 후 종료 시그널 전송
+		time.Sleep(time.Second)
+		quit <- syscall.SIGINT
+	}()
+
+	wg.Add(1)
+	go closeWithContext(ctx, cancel, server, quit, &wg)
+
+	wg.Wait() // 모든 고루틴이 종료될 때까지 대기
+
+	assert.ErrorIs(t, context.Canceled, ctx.Err())
 }
