a505998b23a5e838cfa90eb567ed8b994f305ac7
kjuiop <copying_y@naver.com>
2023-08-17 15:41:01 +0900

    [#2] test: servet http server test code add

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -35,6 +35,49 @@ func TestApiHandler_HealthCheck(t *testing.T) {
 	assert.Equal(t, expectedBody, resp.Body.String())
 }
 
+func TestApiHandler_ServeHttpServer(t *testing.T) {
+
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+
+	cfg := &config.Config{ApiPort: "8080"}
+	ctx, cancel := context.WithCancel(context.Background())
+	handler := NewApiHandler(cfg)
+	router.GET("/health", handler.HealthCheck)
+
+	server := &http.Server{
+		Addr:    ":" + cfg.ApiPort,
+		Handler: router,
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go handler.ServeHttpServer(ctx, server, &wg)
+
+	// 테스트용 HTTP 클라이언트 생성
+	client := http.Client{}
+	req, _ := http.NewRequest("GET", "http://localhost:"+cfg.ApiPort+"/health", nil)
+	resp, err := client.Do(req)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		// 1초 후 종료 시그널 전송
+		time.Sleep(time.Second)
+		quit <- syscall.SIGINT
+	}()
+
+	wg.Add(1)
+	go handler.CloseWithContext(ctx, cancel, server, quit, &wg)
+
+	wg.Wait() // 모든 고루틴이 종료될 때까지 대기
+
+	assert.ErrorIs(t, context.Canceled, ctx.Err())
+}
+
 func TestApiHandler_CloseWithContext(t *testing.T) {
 
 	gin.SetMode(gin.TestMode)
@@ -68,5 +111,5 @@ func TestApiHandler_CloseWithContext(t *testing.T) {
 
 	wg.Wait() // 모든 고루틴이 종료될 때까지 대기
 
-	assert.ErrorIs(t, ctx.Err(), context.Canceled)
+	assert.ErrorIs(t, context.Canceled, ctx.Err())
 }
