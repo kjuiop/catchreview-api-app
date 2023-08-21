36b4f022c4e84b3a67c95bb30e8cb6010f040b56
kjuiop <copying_y@naver.com>
2023-08-21 11:28:56 +0900

    [#9] feat: Member Table 존재여부 기능 구현

--- internal/repository/mysql/mysql_member.go
+++ internal/repository/mysql/mysql_member.go
@@ -4,19 +4,38 @@ import (
 	"catchreview-api-app/domain"
 	"context"
 	"database/sql"
+	"log"
 )
 
 type mysqlMemberRepository struct {
-	Conn *sql.DB
+	Conn      *sql.DB
+	tableName string
 }
 
 func NewMysqlMemberRepository(conn *sql.DB) domain.MemberRepository {
-	return &mysqlMemberRepository{
-		Conn: conn,
+
+	repo := &mysqlMemberRepository{Conn: conn, tableName: "Member"}
+	exist, err := repo.checkExistMemberTable()
+	if err != nil {
+		log.Fatalln("checkExistMemberTable err : ", err)
 	}
+	if exist {
+		return repo
+	}
+
+	return repo
 }
 
 func (repo mysqlMemberRepository) Store(ctx context.Context, m *domain.Member) {
 	//TODO implement me
 	panic("implement me")
+}
+
+func (repo mysqlMemberRepository) checkExistMemberTable() (bool, error) {
+	query := "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name = ?"
+	var count int
+	if err := repo.Conn.QueryRow(query, repo.tableName).Scan(&count); err != nil {
+		return false, err
+	}
+	return count > 0, nil
 }
